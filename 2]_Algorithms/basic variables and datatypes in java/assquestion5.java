12f18627c9079ad8a4a1fc74e3d58d6b85232291
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-10-01 21:56:44 +0530

    Delete 2]_Algorithms/basic variables and datatypes in java directory

--- 2]_Algorithms/basic variables and datatypes in java/assquestion5.java
+++
@@ -1,9 +0,0 @@
-public class assquestion5 {
-    // can $ be used as variable
-    
-    public static void main(String args[])
-    {
-        int $ = 24;
-        System.out.println($);
-    }
-}
--- 2]_Algorithms/basic variables and datatypes in java/basics.java
+++
@@ -1,8 +0,0 @@
-//import java.util.*;
-public class basics
-{
-    public static void main(String args[])
-    {
-        System.out.println("hello world");
-    }
-}
