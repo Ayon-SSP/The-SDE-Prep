01491412b69283c2f9e96528323ca5319b624b88
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-10-01 22:00:48 +0530

    Delete 1]_Data Structures/Strings in java directory

--- 1]_Data Structures/Strings in java/assq2.java
+++
@@ -1,8 +0,0 @@
-public class assq2 {
-    public static void main(String args[]) {
-        String str="ShradhaDidi"; 
-        String str1="ApnaCollege";
-        String str2="ShradhaDidi";
-        System.out.println(str.equals(str1) +" "+str.equals(str2));
-    
-}}
--- 1]_Data Structures/Strings in java/assq3.java
+++
@@ -1,7 +0,0 @@
-public class assq3 {
-    public static void main(String args[])
-     {
-        String str="ApnaCollege".replace("l","");
-     System.out.println(str);}
-    
-}
