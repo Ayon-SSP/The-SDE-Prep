d85355ec8ffe3065641a86e8353e30609d85af15
DARK <80549753+Ayon-SSP@users.noreply.github.com>
2022-10-01 16:52:12 +0530

    Array SDE Sheet 🎞️

---
+++ 4]_SDE Sheet's/Strivers SDE Sheet/C++/Day 01/Set_Matrix_Zeroes.cpp
@@ -0,0 +1,46 @@
+// Time Complexity: O(2*(N*M)), as we are traversing two times in a matrix,
+// Space Complexity: O(1).
+
+#include<bits/stdc++.h>
+using namespace std;
+void setZeroes(vector < vector < int >> & matrix) {
+  int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+  for (int i = 0; i < rows; i++) {
+    //checking if 0 is present in the 0th column or not
+    if (matrix[i][0] == 0) col0 = 0;
+    for (int j = 1; j < cols; j++) {
+      if (matrix[i][j] == 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+  //traversing in the reverse direction and
+  //checking if the row or col has 0 or not
+  //and setting values of matrix accordingly.
+  for (int i = rows - 1; i >= 0; i--) {
+    for (int j = cols - 1; j >= 1; j--) {
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0;
+      }
+    }
+    if (col0 == 0) {
+      matrix[i][0] = 0;
+    }
+
+  }
+
+}
+
+int main() {
+  vector < vector < int >> arr;
+  arr = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+  setZeroes(arr);
+  cout<<"The Final Matrix is "<<endl;
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << "\n";
+  }
+}
