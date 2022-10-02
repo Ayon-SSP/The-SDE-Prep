2e6d5a146f84607df7ea18b9c782326e030b953a
Jordan Hilado <jordanalihilado@gmail.com>
2022-10-01 22:54:53 -0700

    made requested changes

--- 2]_Algorithms/2]_Sorting/mergeSort.c
+++ 2]_Algorithms/2]_Sorting/mergeSort.c
@@ -1,47 +1,3 @@
-#include <stdio.h>
-
-// QUICK SORT
-
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void qsort2(int *a, int n)
-{
-    if (n == 0)
-    {
-        printf("Array is empty.");
-        return;
-    }
-    if (n < 2)
-    {
-        return;
-    }
-    int pivot, left, right, leftLen, rightLen, *leftArr, *rightArr;
-    pivot = *(a);
-    left = 1;
-    for (int right = 1; right <= n; right++)
-    {
-        if (*(a + right) < pivot)
-        {
-            swap((a + left), (a + right));
-            left++;
-        }
-    }
-    swap(a, (a + left - 1));
-    leftLen = left - 1;
-    leftArr = a;
-    rightLen = n - left;
-    rightArr = (a + left);
-    qsort2(rightArr, rightLen);
-    qsort2(leftArr, leftLen);
-};
-
-// MERGE SORT
-
 void msort(int *a, int n)
 {
     if (n == 0)
@@ -86,19 +42,6 @@ void msort(int *a, int n)
 
 int main(void)
 {
-    int arr1[5] = {2, 4, 1, 5, 3};
-    printf("Before Quick Sort:\n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d", arr1[i]);
-    }
-    qsort2(arr1, 5);
-    printf("\nAfter Quick Sort:\n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d", arr1[i]);
-    }
-
     printf("\nBefore Merge Sort:\n");
     int arr2[5] = {5, 4, 1, 2, 3};
     for (int i = 0; i < 5; i++)
