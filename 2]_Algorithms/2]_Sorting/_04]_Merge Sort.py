9f37237146034b9b995eed82597fb825bf729616
Zman-1x1 | サ イ チャラン <saicharankandukuri1x1@gmail.com>
2022-10-08 15:51:50 +0000

    cleanup _ more

--- 2]_Algorithms/2]_Sorting/_04]_Merge Sort.py
+++ 2]_Algorithms/2]_Sorting/_04]_Merge Sort.py
@@ -1,3 +1,42 @@
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst    
+    return merge_two_sorted_lists(merge_sort(lst[:len(lst)//2]), merge_sort(lst[len(lst)//2:]), lst)
+    
+def merge_two_sorted_lists(left,right,merged):
+    left_idx = right_idx = 0
+    
+    while left_idx<len(left) and right_idx<len(right):
+        if(left[left_idx]<=right[right_idx]):
+            merged[left_idx + right_idx] = left[left_idx]
+            left_idx += 1
+        else:
+            merged[left_idx + right_idx] = right[right_idx]
+            right_idx += 1
+    
+    if(left_idx<len(left)):
+        merged[len(right)+left_idx:len(left)+len(right)] = left[left_idx:len(left)]
+        
+    if(right_idx<len(right)):
+        merged[len(left)+right_idx:len(left)+len(right)] = right[right_idx:len(right)]
+        
+    return merged
+
+if __name__ == '__main__':
+    test_cases = [
+        [],
+        [3],
+        [9, 8, 7, 2],
+        [3, 4, 1, 2, 5],
+        [6, 5, 12, 10, 9, 1],
+        [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+        [10, 3, 15, 7, 8, 23, 98, 29]
+    ]
+
+    for lst in test_cases:
+        print("Before : ",lst,end=" ")
+        print(" -> ","After : ", merge_sort(lst))
+
 """
 def merge_sort(lst):
     if len(lst) <= 1:
@@ -53,42 +92,3 @@ if __name__ == '__main__':
         merge_sort(lst)
         print("After : ",lst)
 """
-
-def merge_sort(lst):
-    if len(lst) <= 1:
-        return lst    
-    return merge_two_sorted_lists(merge_sort(lst[:len(lst)//2]), merge_sort(lst[len(lst)//2:]), lst)
-    
-def merge_two_sorted_lists(left,right,merged):
-    left_idx = right_idx = 0
-    
-    while left_idx<len(left) and right_idx<len(right):
-        if(left[left_idx]<=right[right_idx]):
-            merged[left_idx + right_idx] = left[left_idx]
-            left_idx += 1
-        else:
-            merged[left_idx + right_idx] = right[right_idx]
-            right_idx += 1
-    
-    if(left_idx<len(left)):
-        merged[len(right)+left_idx:len(left)+len(right)] = left[left_idx:len(left)]
-        
-    if(right_idx<len(right)):
-        merged[len(left)+right_idx:len(left)+len(right)] = right[right_idx:len(right)]
-        
-    return merged
-
-if __name__ == '__main__':
-    test_cases = [
-        [],
-        [3],
-        [9, 8, 7, 2],
-        [3, 4, 1, 2, 5],
-        [6, 5, 12, 10, 9, 1],
-        [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-        [10, 3, 15, 7, 8, 23, 98, 29]
-    ]
-
-    for lst in test_cases:
-        print("Before : ",lst,end=" ")
-        print(" -> ","After : ", merge_sort(lst))
