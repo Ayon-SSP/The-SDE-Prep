6244fd434d8156f4e9f4471c194ef1f2f69f3eff
Kunal-8789 <mrblazerebel@gmail.com>
2022-10-25 18:02:25 +0530

    Adding Striver's graph question in Striver's Sheet

---
+++ 4]_SDE Sheet's/Strivers SDE Sheet/Day 23 [Graph]/Detect A Cycle in Directed Graph using DFS.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  private:
+    bool checkCycle(int node, vector < int > adj[], int vis[], int dfsVis[]) {
+      vis[node] = 1;
+      dfsVis[node] = 1;
+      for (auto it: adj[node]) {
+        if (!vis[it]) {
+          if (checkCycle(it, adj, vis, dfsVis)) return true;
+        } else if (dfsVis[it]) {
+          return true;
+        }
+      }
+      dfsVis[node] = 0;
+      return false;
+    }
+  public:
+    bool isCyclic(int N, vector < int > adj[]) {
+      int vis[N], dfsVis[N];
+     
+      for(int i = 0; i < N; i++){
+        vis[i] = 0;
+        dfsVis[i] = 0;
+      }
+
+      for (int i = 0; i < N; i++) {
+        if (!vis[i]) {
+          // cout << i << endl; 
+          if (checkCycle(i, adj, vis, dfsVis)) {
+             // cout << i << endl;
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+};
+
+void addEdge(vector < int > adj[], int u, int v) {
+  adj[u].push_back(v);
+}
+
+int main() {
+
+  int V = 6;
+
+  vector < int > adj[V];
+  addEdge(adj, 0, 1);
+  addEdge(adj, 1, 2);
+  addEdge(adj, 1, 5);
+  addEdge(adj, 2, 3);
+  addEdge(adj, 3, 4);
+  addEdge(adj, 4, 0);
+  addEdge(adj, 4, 1);
+  
+
+  Solution obj;
+  if (obj.isCyclic(V, adj))
+    cout << "Cycle Detected" << "\n";
+  else
+    cout << "No Cycle Detected";
+
+  return 0;
+}
+
+/*
+Output:
+Cycle detected
+
+Time Complexity: O(V + E), since in its whole, it is a DFS implementation, V – vertices; E – edges;
+
+Space Complexity: O(V), because, apart from the graph, we have 2 arrays of size V, to store the required information
+*/
