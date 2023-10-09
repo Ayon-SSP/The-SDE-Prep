ce17a6184ca084d008f7de69932d97d2c1b439e1
Error By Night <92923836+bot077@users.noreply.github.com>
2023-10-09 21:07:46 +0530

    Create Merge_Strings_Alternately.py

---
+++ 2]_Algorithms/4]_Strings/Merge_Strings_Alternately.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+
+        return ''.join(result)
